10789709eec07af2b057d1eabdf52afc5da121ba
rauleite <raul.abreu.leite@gmail.com>
2023-03-27 12:03:43 -0300

    turborepo added

--- apps/remix/playwright.config.ts
+++ apps/remix/playwright.config.ts
@@ -75,7 +75,7 @@ const config: PlaywrightTestConfig = {
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: "pnpm run dev",
+    command: "npm run dev",
     url: baseURL,
     reuseExistingServer: !process.env.CI,
   },
